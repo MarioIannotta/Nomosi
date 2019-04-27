fcbc33a90245a0fafc3469b41be8276b5b437c61
Mario Iannotta <info@marioiannotta.com>
2019-04-27 12:09:08 +0200

    Improve ServiceError by providing status code and underlying network error

--- Nomosi/Core/ServiceError.swift
+++ Nomosi/Core/ServiceError.swift
@@ -8,34 +8,56 @@
 
 import Foundation
 
-public struct ServiceError: Error, Equatable {
+public struct ServiceError: Error {
+    
+    public struct UserInfo {
+        
+        public let statusCode: Int?
+        public let underlyingError: Error?
+        
+        fileprivate init(statusCode: Int? = nil, underlyingError: Error? = nil) {
+            self.statusCode = statusCode
+            self.underlyingError = underlyingError
+        }
+    }
     
     public static func invalidStatusCode(_ statusCode: Int?) -> ServiceError {
-        var statusCodeDescription = ""
-        if let statusCode = statusCode {
-            statusCodeDescription = String(statusCode)
-        }
-        return ServiceError(code: 7, reason: "The status code \(statusCodeDescription) is invalid")
+        return ServiceError(code: 7,
+                            reason: "Invalid status code \(String(describing: statusCode))",
+                            userInfo: UserInfo(statusCode: statusCode))
     }
     public static var redundantRequest = ServiceError(code: 6, reason: "The same request is already running")
     public static var requestCancelled = ServiceError(code: 5, reason: "The request has been cancelled")
-    public static var invalidRequest = ServiceError(code: 4, reason: "The request is not valid")
+    public static var invalidRequest = ServiceError(code: 4, reason: "Invalid request")
     public static var emptyResponse = ServiceError(code: 3, reason: "Empty response")
     public static func cannotParseResponse(error: Error) -> ServiceError {
-        return ServiceError(code: 2, reason: "Can't parse the response; Error: \(error)")
+        return ServiceError(code: 2,
+                            reason: "Can't parse the response",
+                            userInfo: UserInfo(underlyingError: error))
     }
     
     public var code: Int
     public var reason: String
+    public var userInfo: UserInfo?
     
-    public init(code: Int, reason: String) {
+    public init(code: Int, reason: String, userInfo: UserInfo? = nil) {
         self.code = code
         self.reason = reason
+        self.userInfo = userInfo
     }
     
     public init(networkError: Error) {
         self.code = 1
         self.reason = networkError.localizedDescription
+        self.userInfo = UserInfo(underlyingError: networkError)
+    }
+    
+}
+
+extension ServiceError: Equatable {
+    
+    public static func == (lhs: ServiceError, rhs: ServiceError) -> Bool {
+        return lhs.code == rhs.code
     }
     
 }
